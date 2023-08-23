f11c14f659248e82683fa98d3a61f6840da849df
tuturu0 <79896742+tuturu0@users.noreply.github.com>
2023-08-24 00:53:19 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash 
 
 cd /home/yc-user && \
+sudo apt update && \
 sudo apt install -y git && \
 git clone -b monolith https://github.com/express42/reddit.git && \
 cd /home/yc-user/reddit && \
