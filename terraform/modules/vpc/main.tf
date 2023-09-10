bfb34407f0efba458bf9abc2d7095a39a6ae8d3a
tuturu0 <79896742+tuturu0@users.noreply.github.com>
2023-09-10 23:53:52 +0300

    Update main.tf

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_version = "1.5.1"
-  required_providers {
-    yandex = {
-      source  = "yandex-cloud/yandex"
-      version = "0.95.0"
-    }
-  }
-}
-
 resource "yandex_vpc_network" "app-network" {
   name = "app-network"
 }
