6489676ba719eeb33c9d864abe065a2804d4ec0c
tuturu0 <79896742+tuturu0@users.noreply.github.com>
2023-09-10 23:54:12 +0300

    Update main.tf

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -1,11 +1,11 @@
-terraform {
-  required_providers {
-    yandex = {
-      source  = "yandex-cloud/yandex"
-      version = "0.95.0"
-    }
-  }
-}
+#terraform {
+#  required_providers {
+#    yandex = {
+#      source  = "yandex-cloud/yandex"
+#      version = "0.95.0"
+#    }
+#  }
+#}
 
 
 resource "yandex_compute_instance" "app" {
